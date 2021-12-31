96d67e6c156ba7235cfacecc0f853f8e852812d5
Nurgul Amat <nurgulamat212@gmail.com>
2021-12-30 23:11:30 -0600

    commit showPage function

--- js/script.js
+++ js/script.js
@@ -2,29 +2,41 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
+// Global variables
+let studentList = document.querySelector(".student-list");
+const itemsPerPage = 9;
+const currentPage = 1;
 
 
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+// showPage function creates and insert/append the elements needed to display a page of nine students
+function showPage(list, page) {
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage -1;
+
+   studentList.innerHTML = '' ;
+
+   for(let i = 0; i < list.length ; i++){
+
+      if(i >= startIndex && i <= endIndex){
+        
+         let studentItem = `
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+                  <h3>${list[i].name.first} ${list[i].name.last} </h3>
+                  <span class="email">${list[i].email}</span>
+               </div>
+
+               <div class="joined-details">
+                  <span class="date">Joined ${list[i].registered.date}</span>
+               </div> 
+            </li>                     
+         `;
+      studentList.insertAdjacentHTML("beforeend", studentItem);
+      }
+   }
+}
+showPage(data, currentPage);
 
 
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
-
-
-// Call functions
